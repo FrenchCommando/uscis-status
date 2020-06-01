bcacef9bceda3a5d8d829eb0cf6c5a85c09476d0
frenchcommando <martialren@gmail.com>
2020-06-01 12:13:29 -0400

    remove obsolete sandbox

--- sandbox/run_pool.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from aiohttp import web
-from src.constants import port_number
-from sandbox.db_pool_stuff import init_app
-
-loop = asyncio.get_event_loop()
-app = loop.run_until_complete(init_app())
-web.run_app(app, port=port_number)
