246132d843647fdb6d737395a817f7c39f99e1f1
frenchcommando <martialren@gmail.com>
2020-06-10 21:46:28 -0400

    numbers change

--- uscis_service/examples/update_cases.py
+++ uscis_service/examples/update_cases.py
@@ -9,7 +9,7 @@ from src.update_functions import update_entries, smart_update_all, refresh_case,
 # asyncio.get_event_loop().run_until_complete(smart_update_all(
 #     prefix="LIN", date_start=20001, index_start=50001, skip_existing=False))
 # asyncio.get_event_loop().run_until_complete(smart_update_all(
-#     prefix="LIN", date_start=20902, index_start=1, skip_existing=False))
+#     prefix="LIN", date_start=20900, index_start=1, skip_existing=False))
 
 # asyncio.get_event_loop().run_until_complete(refresh_case(status='CASE STATUS'))
 # asyncio.get_event_loop().run_until_complete(refresh_selected_status(filter_function=lambda x: 0 < x < 100))
