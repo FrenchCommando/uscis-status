63bf60f25f811a5e08547677eef5767b0f4453e9
frenchcommando <martialren@gmail.com>
2020-06-11 22:20:21 -0400

    db_analysis refactoring - bis

--- uscis_service/examples/db_analysis.py
+++ uscis_service/examples/db_analysis.py
@@ -2,10 +2,11 @@ import asyncio
 from src.db_analysis_functions import summary_analysis, summary_analysis_form, summary_analysis_status
 
 
-asyncio.get_event_loop().run_until_complete(summary_analysis(print_results=True))
-asyncio.get_event_loop().run_until_complete(summary_analysis_form(
-    ref_form="Form I-129, Petition for a Nonimmigrant Worker",
-    print_results=True, buckets_instead_of_strip=True))
-asyncio.get_event_loop().run_until_complete(summary_analysis_status(
-    ref_status="CASE STATUS",
-    print_results=False, buckets_instead_of_strip=False))
+asyncio.get_event_loop().run_until_complete(
+    summary_analysis(print_results=True, buckets_instead_of_strip=True))
+asyncio.get_event_loop().run_until_complete(
+    summary_analysis_form(ref_form="Form I-129, Petition for a Nonimmigrant Worker",
+                          print_results=True, buckets_instead_of_strip=True))
+asyncio.get_event_loop().run_until_complete(
+    summary_analysis_status(ref_status="CASE STATUS",
+                            print_results=False, buckets_instead_of_strip=False))
