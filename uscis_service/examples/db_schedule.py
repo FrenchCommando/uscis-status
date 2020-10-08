97db9ad959bdef4dd5df729e597c181db7595f20
frenchcommando <martialren@gmail.com>
2020-10-08 11:38:10 -0400

    apscheduler seems to work - revert some

--- uscis_service/examples/db_schedule.py
+++ uscis_service/examples/db_schedule.py
@@ -1,12 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from examples.db_batch import refresh_error
+
+
 scheduler = BlockingScheduler()
 
 
-i = 0
-
-
 def main():
+    i = 0
+
     def some_job():
         global i
         print("Decorated job")
@@ -14,7 +15,7 @@ def main():
         i += 1
 
     scheduler.add_job(some_job, 'interval', minutes=1)
-    scheduler.add_job(refresh_error, 'interval', minutes=1)
+    # scheduler.add_job(refresh_error, 'interval', minutes=1)
     scheduler.start()
 
 
