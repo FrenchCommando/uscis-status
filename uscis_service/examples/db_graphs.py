ad0b97396c7b28f151e77f774d7b6f9d140b82b6
FrenchCommando <martialrengmail.com>
2020-11-25 00:51:36 -0500

    db_graphs with networkx

--- uscis_service/examples/db_graphs.py
+++ uscis_service/examples/db_graphs.py
@@ -24,11 +24,57 @@ for old_status, vv in common.items():
     print(old_status)
     print(vv)
 
+
+plt.figure("Status Graph")
 G = nx.DiGraph()
 G.add_weighted_edges_from(ebunch_to_add=[
     (old_status, new_status, number) for old_status, v in common.items() for new_status, number in v.items()
 ])
+nx.set_node_attributes(G=G, name='favorite_color', values={
+    "Case Was Approved": "red",
+})
 for (n, d), (nn, dd), (nnn, ddd) in zip(G.degree(), G.out_degree(), G.in_degree()):
     print(d, dd, ddd, n, nn, nnn)
-nx.draw_shell(G, with_labels=True, font_size=5, node_size=200, width=1)
+print(nx.info(G))
+pos = nx.shell_layout(G=G)
+enhanced_pos = nx.shell_layout(G=G)
+
+
+def direction_consistent(direction=1):
+    for node_from in enhanced_pos:
+        y_from = enhanced_pos[node_from][direction]
+        for node_to in G.successors(n=node_from):
+            y_to = enhanced_pos[node_to][direction]
+            if node_from in G.successors(node_to):
+                # if y_to != y_from:
+                #     print("equal", y_from, y_to, node_from, node_to)
+                #     enhanced_pos[node_from][direction] = min(y_from, y_to)
+                #     enhanced_pos[node_to][direction] = min(y_from, y_to)
+                #     return False
+                continue
+            if y_from <= y_to:
+                # print(y_from, y_to, node_from, node_to, y_from - (y_to - y_from))
+                enhanced_pos[node_to][direction] = y_from - (y_to - y_from)
+                return False
+    return True
+
+
+i = 0
+while not direction_consistent() and i < 100:
+    i += 1
+print(i)
+
+for (n1, p1), (n2, p2) in zip(pos.items(), enhanced_pos.items()):
+    print(p1, p2, n1, n2)
+nx.draw(
+    G,
+    with_labels=True, font_size=5, node_size=200, width=1,
+    pos=enhanced_pos,
+)
+for node, val in G.nodes.data():
+    if "favorite_color" in val:
+        color_value = val["favorite_color"]
+        nx.draw_networkx_nodes(
+            G, enhanced_pos, nodelist=[node], node_color=color_value, node_size=100, alpha=0.8
+        )
 plt.show()
