61f1042902870c6a5298febd3a7e61690cd6c2df
FrenchCommando <martialrengmail.com>
2020-11-25 10:16:53 -0500

    dash wired to graphs

--- uscis_service/examples/db_graphs.py
+++ uscis_service/examples/db_graphs.py
@@ -108,4 +108,103 @@ for node, val in G.nodes.data():
             G, enhanced_pos, nodelist=[node], node_color=color_value, node_size=100, alpha=0.8
         )
 
-plt.show()
+# plt.show()
+
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+def build_figure_from_graph(G, pos):
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            # colorscale options
+            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+            colorscale='YlGnBu',
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    node_adjacencies = []
+    node_text = []
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_adjacencies.append(len(adjacencies[1]))
+        node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+    node_trace.marker.color = node_adjacencies
+    node_trace.text = node_text
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                    title='<br>Network graph made with Python',
+                    titlefont_size=16,
+                    showlegend=False,
+                    hovermode='closest',
+                    margin=dict(b=20,l=5,r=5,t=40),
+                    annotations=[ dict(
+                        text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                        showarrow=False,
+                        xref="paper", yref="paper",
+                        x=0.005, y=-0.002 ) ],
+                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+    return fig
+
+
+graphs = [
+    dcc.Graph(figure=build_figure_from_graph(G=G, pos=enhanced_pos))
+]
+
+if "subG" in locals():
+    graphs.append(dcc.Graph(figure=build_figure_from_graph(G=subG, pos=enhanced_pos)))
+    graphs.append(dcc.Graph(figure=build_figure_from_graph(G=subG, pos=nx.shell_layout(G=subG))))
+
+
+app = dash.Dash()
+app.layout = html.Div(graphs)
+
+server = app.server
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False, port=8850, host="0.0.0.0")
