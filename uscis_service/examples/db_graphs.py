69dc4db01a1856b322dfa6a000098e8c21fcf17c
FrenchCommando <martialrengmail.com>
2020-11-25 20:52:51 -0500

    db_graphs with networkx - refactoring - no matplotlib

--- uscis_service/examples/db_graphs.py
+++ uscis_service/examples/db_graphs.py
@@ -1,195 +1,37 @@
-import pandas as pd
-import requests
-import json
-import networkx as nx
-# import matplotlib.pyplot as plt
-from src.constants import port_number, host_uscis_service
-import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
+import networkx as nx
 import dash_html_components as html
+from src.graph_stuff import UscisGraph, GraphCommon
 
+ug = UscisGraph()
+ug.describe()
 
-r = requests.get(f'http://{host_uscis_service}:{port_number}/graph')
-data = json.loads(r.text)
+G = ug.digraph()
 
-print(f"Number of Items:\t{data['number_of_items']}")
-graph = data['graph']
-print(f"Graph\t{graph}")
-
-for form, v in graph.items():
-    print(f"Form:\t{sum(sum(vv.values()) for vv in v.values())}\t{form}")
-
-common = graph["Common"]
-# common = graph["Form I-129, Petition for a Nonimmigrant Worker"]
-# common = graph["Form I-290B, Notice of Appeal or Motion"]
-
-for old_status, vv in common.items():
-    print(old_status)
-    print(vv)
-
-G = nx.DiGraph()
-G.add_weighted_edges_from(ebunch_to_add=[
-    (old_status, new_status, number) for old_status, v in common.items() for new_status, number in v.items()
-])
 nx.set_node_attributes(G=G, name='favorite_color', values={
     "Case Was Approved": "red",
     "Case Was Received": "green",
     "Case Was Approved And My Decision Was Emailed": "orange",
     "Case Was Denied": "magenta",
-
 })
-for (n, d), (nn, dd), (nnn, ddd) in zip(G.degree(), G.out_degree(), G.in_degree()):
-    print(d, dd, ddd, n, nn, nnn)
-print(nx.info(G))
-pos = nx.shell_layout(G=G)
-enhanced_pos = nx.shell_layout(G=G)
 
+GraphCommon.describe_graph_degree(g=G)
 
-def direction_consistent(direction=1):
-    for node_from in enhanced_pos:
-        y_from = enhanced_pos[node_from][direction]
-        for node_to in G.successors(n=node_from):
-            y_to = enhanced_pos[node_to][direction]
-            if node_from in G.successors(node_to):
-                # if y_to != y_from:
-                #     print("equal", y_from, y_to, node_from, node_to)
-                #     enhanced_pos[node_from][direction] = min(y_from, y_to)
-                #     enhanced_pos[node_to][direction] = min(y_from, y_to)
-                #     return False
-                continue
-            if y_from <= y_to:
-                print(y_from, y_to, node_from, node_to, y_from - (y_to - y_from))
-                enhanced_pos[node_to][direction] = y_from - (y_to - y_from)
-                return False
-    return True
-
-
-i = 0
-while not direction_consistent() and i < 100:
-    i += 1
-print(i)
-
-for (n1, p1), (n2, p2) in zip(pos.items(), enhanced_pos.items()):
-    print(p1, p2, n1, n2)
-
-if i == 100:
-    i = 0
-    while not direction_consistent(direction=0) and i < 100:
-        i += 1
-
-    subG = G.subgraph(nodes=(n for n in enhanced_pos if enhanced_pos[n][1] < -20))
-
-    def draw_internal(sub_layout):
-        nx.draw(
-            subG,
-            with_labels=True, font_size=5, node_size=200, width=1,
-            pos=sub_layout,
-        )
-        for node, val in subG.nodes.data():
-            if "favorite_color" in val:
-                color_value = val["favorite_color"]
-                nx.draw_networkx_nodes(
-                    G, sub_layout, nodelist=[node], node_color=color_value, node_size=100, alpha=0.8
-                )
-
-    # plt.figure(num="Cycle subset - Raw")
-    draw_internal(sub_layout=enhanced_pos)
-
-    # plt.figure(num="Cycle subset - Circle")
-    draw_internal(sub_layout=nx.shell_layout(G=subG))
-
-# plt.figure(num="Status Oriented Graph")
-nx.draw(
-    G,
-    with_labels=True, font_size=5, node_size=200, width=1,
-    pos=enhanced_pos,
-)
-for node, val in G.nodes.data():
-    if "favorite_color" in val:
-        color_value = val["favorite_color"]
-        nx.draw_networkx_nodes(
-            G, enhanced_pos, nodelist=[node], node_color=color_value, node_size=100, alpha=0.8
-        )
-
-# plt.show()
-
-
-def build_figure_from_graph(G, pos, title):
-    edge_x = []
-    edge_y = []
-    for edge in G.edges():
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
-
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines')
-
-    node_x = []
-    node_y = []
-    for node in G.nodes():
-        x, y = pos[node]
-        node_x.append(x)
-        node_y.append(y)
-
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers',
-        hoverinfo='text',
-        marker=dict(
-            showscale=True,
-            colorscale='YlGnBu',
-            reversescale=True,
-            color=[],
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
-            line_width=2))
-
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-        node_text.append('# of connections: ' + str(len(adjacencies[1])))
-
-    node_trace.marker.color = node_adjacencies
-    node_trace.text = node_text
-
-    fig = go.Figure(data=[edge_trace, node_trace],
-                    layout=go.Layout(
-                    title=f'<br>{title}',
-                    titlefont_size=16,
-                    showlegend=False,
-                    hovermode='closest',
-                    margin=dict(b=20, l=5, r=5, t=40),
-                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-                    )
-    return fig
+enhanced_pos = GraphCommon.find_layout(g=G)
+sub_g = GraphCommon.problematic_subgraph(g=G)
+even_better_pos = GraphCommon.find_even_better_layout(g=G)
 
 
 graphs = [
-    dcc.Graph(figure=build_figure_from_graph(G=G, pos=enhanced_pos, title="Status Oriented Graph"))
+    dcc.Graph(figure=GraphCommon.build_figure_from_graph(G=G, pos=enhanced_pos, title="Status Oriented Graph"))
 ]
 
-if "subG" in locals():
-    graphs.append(dcc.Graph(figure=build_figure_from_graph(
-        G=subG, pos=enhanced_pos, title="Cycle subset - Raw")))
-    graphs.append(dcc.Graph(figure=build_figure_from_graph(
-        G=subG, pos=nx.shell_layout(G=subG), title="Cycle subset - Circle")))
+if "sub_g" in locals():
+    graphs.append(dcc.Graph(figure=GraphCommon.build_figure_from_graph(
+        G=sub_g, pos=enhanced_pos, title="Cycle subset - Raw")))
+    graphs.append(dcc.Graph(figure=GraphCommon.build_figure_from_graph(
+        G=sub_g, pos=nx.shell_layout(G=sub_g), title="Cycle subset - Circle")))
 
 
 app = dash.Dash()
---
+++ uscis_service/src/graph_stuff.py
@@ -0,0 +1,154 @@
+import json
+import networkx as nx
+import requests
+import plotly.graph_objects as go
+from src.constants import port_number, host_uscis_service
+
+
+class UscisGraph:
+    def __init__(self):
+        r = requests.get(f'http://{host_uscis_service}:{port_number}/graph')
+        data = json.loads(r.text)
+        self.graph = data['graph']
+        self.number_of_items = data['number_of_items']
+        self.forms = list(self.graph.keys())
+        self.form_count = {form: sum(sum(vv.values()) for vv in v.values()) for form, v in self.graph.items()}
+
+    def describe(self):
+        print(f"Number of Items:\t{self.number_of_items}")
+        print(f"Graph:\t{self.graph}")
+        for f in self.forms:
+            print(f"Form:\t{self.form_count[f]:9d}\t{f}")
+
+    def digraph(self, form="Common"):
+        if form not in self.forms:
+            raise ValueError("UscisGraph - digraph - form not in forms")
+        common = self.graph[form]
+        g = nx.DiGraph()
+        g.add_weighted_edges_from(ebunch_to_add=[
+            (old_status, new_status, number) for old_status, v in common.items() for new_status, number in v.items()
+        ])
+        return g
+
+
+class GraphCommon:
+    @staticmethod
+    def describe_graph_degree(g):
+        for (n, d), (nn, dd), (nnn, ddd) in zip(g.degree(), g.out_degree(), g.in_degree()):
+            print(d, dd, ddd, n, nn, nnn)
+        print(nx.info(g))
+
+    @staticmethod
+    def problematic_subgraph(g):
+        enhanced_pos = GraphCommon.find_layout(g=g)
+        sub_g = g.subgraph(nodes=(n for n in enhanced_pos if enhanced_pos[n][1] < -20))
+        return sub_g
+
+    @staticmethod
+    def direction_consistent(g, pos, direction):
+        for node_from in pos:
+            y_from = pos[node_from][direction]
+            for node_to in g.successors(n=node_from):
+                y_to = pos[node_to][direction]
+                if node_from in g.successors(node_to):
+                    continue
+                if y_from <= y_to:
+                    # print(y_from, y_to, node_from, node_to, y_from - (y_to - y_from))
+                    pos[node_to][direction] = y_from - (y_to - y_from)
+                    return False
+        return True
+
+    @staticmethod
+    def find_layout(g):
+        enhanced_pos = nx.shell_layout(G=g)
+        i = 0
+        i_max = 100
+        while not GraphCommon.direction_consistent(g=g, pos=enhanced_pos, direction=1) and i < i_max:
+            i += 1
+        return enhanced_pos
+
+    @staticmethod
+    def find_even_better_layout(g):
+        return nx.shell_layout(G=g)
+
+    @staticmethod
+    def draw_internal(sub_g, sub_layout):  # used in conjonction with matplotlib
+        nx.draw(
+            sub_g,
+            with_labels=True, font_size=5, node_size=200, width=1,
+            pos=sub_layout,
+        )
+        for node, val in sub_g.nodes.data():
+            if "favorite_color" in val:
+                color_value = val["favorite_color"]
+                nx.draw_networkx_nodes(
+                    sub_g, sub_layout, nodelist=[node], node_color=color_value, node_size=100, alpha=0.8
+                )
+
+    @staticmethod
+    def build_figure_from_graph(G, pos, title):
+        edge_x = []
+        edge_y = []
+        for edge in G.edges():
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            edge_x.append(x0)
+            edge_x.append(x1)
+            edge_x.append(None)
+            edge_y.append(y0)
+            edge_y.append(y1)
+            edge_y.append(None)
+
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        node_x = []
+        node_y = []
+        for node in G.nodes():
+            x, y = pos[node]
+            node_x.append(x)
+            node_y.append(y)
+
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=True,
+                colorscale='YlGnBu',
+                reversescale=True,
+                color=[],
+                size=10,
+                colorbar=dict(
+                    thickness=15,
+                    title='Node Connections',
+                    xanchor='left',
+                    titleside='right'
+                ),
+                line_width=2))
+
+        node_adjacencies = []
+        node_text = []
+        for node, adjacencies in enumerate(G.adjacency()):
+            node_adjacencies.append(len(adjacencies[1]))
+            node_text.append('# of connections: ' + str(len(adjacencies[1])))
+
+        node_trace.marker.color = node_adjacencies
+        node_trace.text = node_text
+
+        fig = go.Figure(
+            data=[edge_trace, node_trace],
+            layout=go.Layout(
+                title=f'<br>{title}',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20, l=5, r=5, t=40),
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+            )
+        )
+        return fig
